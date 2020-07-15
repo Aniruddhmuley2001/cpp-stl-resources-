31266bcad15363b451355332448fb7d259116d18
Aniruddh Muley <54072399+Aniruddhmuley2001@users.noreply.github.com>
2020-07-15 17:34:34 +0530

    Create sieve_of_eratosthenes.cpp

---
+++ sieve_of_eratosthenes.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	vector<bool> is_prime(N+1, true);   //N+1 size to access is_prime[N]
+	
+	for(int i = 2; i*i <= N; i++){
+	    if(is_prime[i]){
+	        for(int j = i+i; j <= N; j+= i)
+	            is_prime[j] = false;
+	    }
+	}
+	
+	for(int i = 2; i <= N; i++){
+	    if(is_prime[i])
+	        cout << i << " ";
+	}
+	
+	return 0;
+}
